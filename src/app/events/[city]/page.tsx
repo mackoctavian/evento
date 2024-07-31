b67604d5f7f07ecbb773faf3c14dbd3138ab800c
Mack Octavian <maestrooctavian@gmail.com>
2024-08-01 02:34:14 +0300

    More styling

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,6 +1,10 @@
+import H1 from "@/components/h1";
+
 export default function EventsPage() {
     return (
-      <main>events</main>
+      <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+        <H1>All events</H1>
+      </main>
     )
   }
   
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Logo() {
   return (
-    <Image src="https://bytegrad.com/course-assets/react-nextjs/evento.png" alt="evento logo" height={12} width={53}></Image>
+    <Link href="/">
+        <Image src="https://bytegrad.com/course-assets/react-nextjs/evento.png" alt="evento logo" height={12} width={53}></Image>
+    </Link>
   )
 }
